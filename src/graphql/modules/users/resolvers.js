3aa8cadfd714462fc5cc18376daf0258277247b8
Arthurgallina1 <arthur.gallina@gmail.com>
2020-09-05 22:03:47 -0300

    add resolvers and mergers

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,4 +1,21 @@
+import User from "../../../models/User";
+
 export default {
-    Query: {},
-    Mutation: {},
+    User: {
+        fullName: (user) => `${user.firstName} ${user.lastName}`,
+    },
+    Query: {
+        users: () => User.find(),
+        user: (_, { id }) => User.findById(id),
+    },
+    Mutation: {
+        createUser: (_, { data }) => User.create(data),
+        updateUser: (_, { id, data }) =>
+            User.findOneAndUpdate(id, data, { new: true }),
+        deleteUser: async (_, { id }) => {
+            const deleted = await User.findOneAndDelete(id);
+            return !!deleted;
+            // forçar um booleano
+        },
+    },
 };
